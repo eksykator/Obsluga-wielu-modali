b9263726a28f3bcace1dc259e302d12e870c662e
Marta Bobowska <martabobowska2@interia.pl>
2018-06-02 22:04:30 +0200

    Remove .show from all modals and add .show to specified modal

--- scripts.js
+++ scripts.js
@@ -1,39 +1,42 @@
 'use strict';
- 
 	
+var showModal = function(event) {
+    event.preventDefault();
+    document.querySelector('#modal-overlay').classList.add('show');
+        
+    var modals = document.querySelectorAll('.modal');
+    for (var i = 0; i < modals.length; i++) {
+         modals[i].classList.remove('show');
+    }
+         
+    var id = this.getAttribute('href');
+    var modalToShow = document.getElementById(id);
+    modalToShow.classList.add('show');
+};
 	
-	var showModal = function(event){
-		event.preventDefault();
-		document.querySelector('#modal-overlay').classList.add('show');
-	};
+var modalLinks = document.querySelectorAll('.show-modal');
 	
-	
-	var modalLinks = document.querySelectorAll('.show-modal');
-	
-	for(var i = 0; i < modalLinks.length; i++){
-		modalLinks[i].addEventListener('click', showModal);
-	}
-	
+for (var i = 0; i < modalLinks.length; i++) {
+    modalLinks[i].addEventListener('click', showModal);
+}
 
-	var hideModal = function(event){
-		event.preventDefault();
-		document.querySelector('#modal-overlay').classList.remove('show');
-	};
+var hideModal = function(event) {
+    event.preventDefault();
+    document.querySelector('#modal-overlay').classList.remove('show');
+};
 	
-	var closeButtons = document.querySelectorAll('.modal .close');
+var closeButtons = document.querySelectorAll('.modal .close');
 	
-	for(var i = 0; i < closeButtons.length; i++){
-		closeButtons[i].addEventListener('click', hideModal);
-	}
+for (var i = 0; i < closeButtons.length; i++) {
+    closeButtons[i].addEventListener('click', hideModal);
+}
 	
-	
-	document.querySelector('#modal-overlay').addEventListener('click', hideModal);
+document.querySelector('#modal-overlay').addEventListener('click', hideModal);
 
+var modals = document.querySelectorAll('.modal');
 	
-	var modals = document.querySelectorAll('.modal');
-	
-	for(var i = 0; i < modals.length; i++){
-		modals[i].addEventListener('click', function(event){
-			event.stopPropagation();
-		});
-	}
+for (var i = 0; i < modals.length; i++) {
+    modals[i].addEventListener('click', function(event){
+        event.stopPropagation();
+    });
+}
